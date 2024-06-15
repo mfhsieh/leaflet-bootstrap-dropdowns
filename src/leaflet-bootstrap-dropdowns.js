b4482487cd6a7f43120c2a0140eb69cd7a65ffc1
mfhsieh <mfhsieh@gmail.com>
2024-06-15 08:24:04 +0800

    v0.1

--- src/leaflet-bootstrap-dropdowns.js
+++ src/leaflet-bootstrap-dropdowns.js
@@ -15,13 +15,13 @@
  */
 (function (factory) {
 
-    if (typeof define === 'function' && define.amd) {
+    if (typeof define === 'function' && define.amd) {  // eslint-disable-line no-undef
         // define an AMD module that relies on 'leaflet'
-        define(['leaflet'], factory);
+        define(['leaflet'], factory);  // eslint-disable-line no-undef
 
     } else if (typeof exports === 'object') {
         // define a Common JS module that relies on 'leaflet'
-        module.exports = factory(require('leaflet'));
+        module.exports = factory(require('leaflet'));  // eslint-disable-line no-undef
 
     } else if (typeof window !== 'undefined') {
         // attach your plugin to the global 'L' variable
